86f266f7d3b7204e8ed537c0d0b440a449ad91aa
Archit <jainarchit1604@gmail.com>
2025-04-01 20:40:50 +0530

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,18 +4,30 @@ import Header from "@/components/header";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 export const metadata = {
   title: "Settle Mitra",
-  description: "One Solution to all your finical problems",
+  description: "One Solution to all your financial problems.",
   icons: {
-    icon: "/logowbg.png",
+    icon: "/favicon.ico",
   },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/* <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head> */}
+      <Head>
+        <title>Settle Mitra</title>
+        <meta
+          name="description"
+          content="One Solution to all your financial problems."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Toaster />
         <WelcomePopup />
